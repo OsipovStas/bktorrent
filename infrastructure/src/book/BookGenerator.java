1c760378b9dcafd6a13d3e9317334288350a435d
OsipovStas <stasstels@gmail.com>
2013-05-12 23:41:00 +0400

    BookGenerator changed

--- infrastructure/src/book/BookGenerator.java
+++ infrastructure/src/book/BookGenerator.java
@@ -11,7 +11,6 @@ import java.nio.file.Paths;
  */
 public final class BookGenerator {
 
-    public static long idCounter = 0;
 
     /**
      * Main method
@@ -19,19 +18,21 @@ public final class BookGenerator {
      * @param args must be {[/output/directories]}
      */
     public static void main(String[] args) {
-        for (String arg : args) {
-            long bookId = ++idCounter;
-            Path bookPath = Paths.get(arg, bookId + ".bk");
-            Path torrentPath = Paths.get(arg, bookId + ".tr");
-            Book book = Book.generateRandomBook(bookId);
-            try {
-                book.dump(bookPath.toFile());
-                book.getHeader().dump(torrentPath.toFile().getAbsolutePath());
-                Book.read(bookPath.toFile());
-                BookHeader.read(torrentPath.toFile().getAbsolutePath());
-            } catch (IOException | ClassNotFoundException e) {
-                e.printStackTrace();
-            }
+        if (args.length != 2) {
+            return;
+        }
+        String bookDir = args[0];
+        long bookId = Long.parseLong(args[1]);
+        Path bookPath = Paths.get(bookDir, bookId + ".bk");
+        Path torrentPath = Paths.get(bookDir, bookId + ".tr");
+        Book book = Book.generateRandomBook(bookId);
+        try {
+            book.dump(bookPath.toFile());
+            book.getHeader().dump(torrentPath.toFile().getAbsolutePath());
+            Book.read(bookPath.toFile());
+            BookHeader.read(torrentPath.toFile().getAbsolutePath());
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
         }
     }
 
